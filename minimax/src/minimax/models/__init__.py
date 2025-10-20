fd9c5cc27244b704ea435b9f8b8535ae15db3260
gabrbrr <pamfil.1938844@studenti.uniroma1.it>
2025-10-20 17:24:45 +0000

    Add minimax as regular folder

---
+++ minimax/src/minimax/models/__init__.py
@@ -0,0 +1,27 @@
+"""
+Copyright (c) Meta Platforms, Inc. and affiliates.
+All rights reserved.
+
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
+from .registration import register, make
+
+
+from .maze import (
+	GridWorldACStudentModel, 
+	GridWorldACTeacherModel,
+)
+from .ltlenv import (
+	ACModel
+)
+
+
+__all__ = [
+	register,
+	make,
+	GridWorldACStudentModel,
+	GridWorldACTeacherModel,
+	ACModel
+]
